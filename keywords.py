a8f7a2b6eff6d60c6fe160ef1d9c582414272c3f
2ShKiPeR <igormokhnatov78@gmail.com>
2025-06-02 14:27:45 +0400

    fix sentiment

--- keywords.py
+++ keywords.py
@@ -13,6 +13,6 @@ class TextRequest(BaseModel):
 def extract_keywords(req: TextRequest):
     keywords = rake.apply(req.text)
     phrases = [kw[0] for kw in keywords]
-    count = dict(Counter(phrases))
-    sorted_result = dict(sorted(count.items(), key=lambda x: -x[1]))
-    return {"keywords": sorted_result}
+    count = Counter(phrases)
+
+    return [{"word": k, "frequency": v} for k, v in count.items()]
